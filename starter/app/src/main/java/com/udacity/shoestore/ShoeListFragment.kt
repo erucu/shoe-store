5db64a176e5f3cd5e25e552a9cfb25455595fdc0
Abdulkadir Erucu <abdulkadir.erucu@telekom.de>
2023-02-07 10:52:20 +0100

    Step 14: Mentor solution

--- starter/app/src/main/java/com/udacity/shoestore/ShoeListFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/ShoeListFragment.kt
@@ -14,41 +14,33 @@ import com.udacity.shoestore.databinding.ItemShoeBinding
 
 
 class ShoeListFragment : Fragment() {
-
     private lateinit var viewModel: ShoeViewModel
-    private lateinit var binding: FragmentShoeDetailBinding
-
+    private lateinit var binding: FragmentShoeListBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding: FragmentShoeListBinding =
+        binding =
             DataBindingUtil.inflate(inflater, R.layout.fragment_shoe_list, container, false)
         // Inflate the layout for this fragment
         viewModel = ViewModelProvider(this).get(ShoeViewModel::class.java)
-
         binding.floatingActionButton.setOnClickListener { view: View ->
             Navigation.findNavController(view)
                 .navigate(R.id.action_shoeListFragment_to_shoeDetailFragment)
         }
-
         for (i in 0..5) {
             val temporaryShoeObject = Shoe(shoeName = "SampleShoe_$i")
-            val itemShoeBinding =
-                ItemShoeBinding.inflate(layoutInflater, binding.shoeElementList, false)
+            val itemShoeBinding = ItemShoeBinding.inflate(layoutInflater, binding.shoeElementList, false)
             itemShoeBinding.shoe = temporaryShoeObject
             binding.shoeElementList.addView(itemShoeBinding.root)
         }
-
         setHasOptionsMenu(true)
         return binding.root
     }
-
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
         inflater.inflate(R.menu.overflow_menu, menu)
     }
-
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return NavigationUI.onNavDestinationSelected(item, requireView().findNavController())
                 || super.onOptionsItemSelected(item)
